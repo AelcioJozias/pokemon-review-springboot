1478c161f2ae269f04997764a9d7e03892818ad7
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-04-21 14:33:28 -0300

    [Security] - users and roles

---
+++ src/main/java/com/pokemonreview/api/models/Role.java
@@ -0,0 +1,18 @@
+package com.pokemonreview.api.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+}
---
+++ src/main/java/com/pokemonreview/api/models/UserEntity.java
@@ -0,0 +1,29 @@
+package com.pokemonreview.api.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
+
+
+}
