cc77ba2d721b507b9bef81a1fbf2958570c2b145
Aelcio Jozias <aelciojoziasp@gmail.com>
2024-05-12 17:00:57 -0300

    [unit-tests] - testing reviewService

--- src/main/java/com/pokemonreview/api/models/Review.java
+++ src/main/java/com/pokemonreview/api/models/Review.java
@@ -2,6 +2,7 @@ package com.pokemonreview.api.models;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Builder
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
---
+++ src/test/java/com/pokemonreview/api/service/ReviewServiceTests.java
@@ -0,0 +1,130 @@
+package com.pokemonreview.api.service;
+
+import com.pokemonreview.api.dto.ReviewDto;
+import com.pokemonreview.api.exceptions.ReviewNotFoundException;
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.Review;
+import com.pokemonreview.api.repository.PokemonRepository;
+import com.pokemonreview.api.repository.ReviewRepository;
+import com.pokemonreview.api.service.impl.ReviewServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ReviewServiceTests {
+
+    @Mock
+    private ReviewRepository reviewRepository;
+    @Mock
+    private PokemonRepository pokemonRepository;
+    @InjectMocks
+    private ReviewServiceImpl reviewService;
+
+    private Pokemon pokemon;
+    private Review review;
+    private ReviewDto reviewDto;
+
+    @BeforeEach
+    public void init() {
+        pokemon = Pokemon.builder().id(1).name("pikachu").type("electric").build();
+        review = Review.builder().title("title").content("content").stars(5).pokemon(pokemon).build();
+        pokemon.setReviews(Collections.singletonList(review));
+        reviewDto = ReviewDto.builder().title("review title").content("test content").stars(5).build();
+    }
+
+    @Test
+    public void ReviewService_CreateReview_ReturnsReviewDto() {
+
+        when(pokemonRepository.findById(Mockito.anyInt())).thenReturn(Optional.of(pokemon));
+        when(reviewRepository.save(Mockito.any(Review.class))).thenReturn(review);
+
+        ReviewDto savedReview = reviewService.createReview(pokemon.getId(), reviewDto);
+
+        Assertions.assertThat(savedReview).isNotNull();
+    }
+
+    @Test
+    public void ReviewService_GetReviewsByPokemonId_ReturnReviewDto() {
+        int reviewId = 1;
+        when(reviewRepository.findByPokemonId(reviewId)).thenReturn(Collections.singletonList(review));
+
+        List<ReviewDto> pokemonReturn = reviewService.getReviewsByPokemonId(reviewId);
+
+        Assertions.assertThat(pokemonReturn).isNotNull();
+    }
+
+    @Test
+    public void ReviewService_GetReviewById_ReturnReviewDto() {
+        int reviewId = 1;
+        int pokemonId = 1;
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.of(pokemon));
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+        ReviewDto reviewReturn = reviewService.getReviewById(reviewId, pokemonId);
+
+        Assertions.assertThat(reviewReturn).isNotNull();
+        Assertions.assertThat(reviewReturn).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Throw exception when the relation of pokemon and review isn't true")
+    public void ReviewService_GetReviewById_ThrowException_ReviewNotFoundException() {
+        int reviewId = 1;
+        int pokemonId = 2;
+
+        review.setPokemon(new Pokemon());
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.of(pokemon));
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+        assertThrows(ReviewNotFoundException.class, () -> {
+            reviewService.getReviewById(1, 2);
+        }, "This review does not belong to a pokemon ");
+
+    }
+
+    @Test
+    public void ReviewService_UpdatePokemon_ReturnReviewDto() {
+        int pokemonId = 1;
+        int reviewId = 1;
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.of(pokemon));
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+        when(reviewRepository.save(review)).thenReturn(review);
+
+        ReviewDto updateReturn = reviewService.updateReview(pokemonId, reviewId, reviewDto);
+
+        Assertions.assertThat(updateReturn).isNotNull();
+    }
+
+    @Test
+    public void ReviewService_DeletePokemonById_ReturnVoid() {
+        int pokemonId = 1;
+        int reviewId = 1;
+
+        review.setPokemon(pokemon);
+
+        when(pokemonRepository.findById(pokemonId)).thenReturn(Optional.of(pokemon));
+        when(reviewRepository.findById(reviewId)).thenReturn(Optional.of(review));
+
+        assertAll(() -> reviewService.deleteReview(pokemonId, reviewId));
+    }
+
+
+}
