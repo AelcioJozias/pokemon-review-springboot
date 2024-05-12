c965dc03d98e4736b7c3586c310e772cdd0340ec
AelcioJozias <aelciojoziasputzel18@gmail.com>
2024-05-12 20:21:05 -0300

    [unit-tests] - testing controller, create pokemon

---
+++ src/test/java/com/pokemonreview/api/controller/PokemonControllerTests.java
@@ -0,0 +1,73 @@
+package com.pokemonreview.api.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pokemonreview.api.controllers.PokemonController;
+import com.pokemonreview.api.dto.PokemonDto;
+import com.pokemonreview.api.dto.ReviewDto;
+import com.pokemonreview.api.models.Pokemon;
+import com.pokemonreview.api.models.Review;
+import com.pokemonreview.api.service.PokemonService;
+import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+@WebMvcTest(controllers = PokemonController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class PokemonControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PokemonService pokemonService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private Pokemon pokemon;
+    private Review review;
+    private ReviewDto reviewDto;
+    private PokemonDto pokemonDto;
+
+    @BeforeEach
+    public void init() {
+        pokemon = Pokemon.builder().id(1).name("pikachu").type("electric").build();
+        review = Review.builder().title("title").content("content").stars(5).pokemon(pokemon).build();
+        pokemon.setReviews(Collections.singletonList(review));
+        reviewDto = ReviewDto.builder().title("review title").content("test content").stars(5).build();
+        pokemonDto = PokemonDto.builder().id(1).name("pickachu").type("electric").build();
+    }
+
+    @Test
+    public void PokemonController_CreatePokemon_ReturnCreated() throws Exception {
+//        when(pokemonService.createPokemon(any())).thenReturn(pokemonDto);
+        given(pokemonService.createPokemon(any(PokemonDto.class))).willAnswer((invocation -> invocation.getArgument(0)));
+        ResultActions response = mockMvc.perform(post("/api/pokemon/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(pokemonDto))
+        );
+
+        response.andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", CoreMatchers.is(pokemonDto.getName())))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.type", CoreMatchers.is(pokemonDto.getType())));
+    }
+
+}
